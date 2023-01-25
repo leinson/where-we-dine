3aa56ff1e28de95931b15caad53e572b51c47b02
leinson <hannah.leinson@gmail.com>
2023-01-25 14:54:11 +0200

    login and register works

--- routes.py
+++ routes.py
@@ -1,9 +1,41 @@
 from flask import redirect, render_template, request
 from app import app
-import reviews
+import users, reviews
 
 
 @app.route("/")
 def index():
     list = reviews.get_reviews()
     return render_template("index.html", count=len(list), reviews=list)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Wrong username or password")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="The passwords are not the same")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Registeration unsuccessful")
---
+++ users.py
@@ -0,0 +1,33 @@
+from db import db
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login(username, password):
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return True
+        else:
+            return False
+
+def logout():
+    del session["user_id"]
+
+def register(username, password):
+    hash_value = generate_password_hash(password)
+    isadmin = "false"
+    try:
+        sql = "INSERT INTO users (username,password,admin) VALUES (:username,:password,:admin)"
+        db.session.execute(sql, {"username":username, "password":hash_value, "admin":isadmin})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+def user_id():
+    return session.get("user_id",0)
